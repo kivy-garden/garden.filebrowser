74ede7c9a47d1de079a58f5aa3275858659145d3
Matthew Einhorn <moiein2000@gmail.com>
2013-05-01 20:40:17 -0400

    update docs

--- filebrowser.py
+++ filebrowser.py
@@ -2,7 +2,16 @@
 FileBrowser
 ======
 
-The :class:`FileBrowser` widget is an advanced file browser.
+The :class:`FileBrowser` widget is an advanced file browser. You use it
+similarly to FileChooser usage.
+
+When opened, it provides a shortcut bar with links to special and system
+directories. When touching next to a shortcut in the links bar, it'll
+expand and show all the directories within that directory. It also allows
+to specify paths to be added to the shortcuts list.
+
+It provides a icon and list view to choose file from. And it accepts
+filter and filename inputs.
 
 '''
 
