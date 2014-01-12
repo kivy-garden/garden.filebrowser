f3f669a7de8fb74aedd479395fc7763e009d263c
Matthew Einhorn <moiein2000@gmail.com>
2014-01-12 08:52:47 -0500

    Bump to 1.1-dev.

--- __init__.py
+++ __init__.py
@@ -52,7 +52,7 @@ a shortcut to the Documents directory added to the favorites bar::
 '''
 
 __all__ = ('FileBrowser', )
-__version__ = '1.0'
+__version__ = '1.1-dev'
 
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.treeview import TreeViewLabel, TreeView
